7cc0fc1c23f177ddbc13f4383944e637db55b215
fabian <Fab.heck@t-online.de>
2019-12-16 20:24:08 +0100

    Add ios content to comparison screen

--- ios/Playground/Screens/Comparison/ComparisonViewController.swift
+++ ios/Playground/Screens/Comparison/ComparisonViewController.swift
@@ -11,10 +11,52 @@ import UIKit
 
 class ComparisonViewController : UIViewController {
     
+    let averageValue: Float = 0.2
+    let useCaseNames: [String] = [
+        NSLocalizedString("nasa_title", comment: ""),
+        NSLocalizedString("fibonacci_title", comment: ""),
+        NSLocalizedString("notes_title", comment: ""),
+        NSLocalizedString("pixelsort_title", comment: ""),
+        NSLocalizedString("settings_title", comment: "")
+    ]
+    let useCaseValues: [Float] = [0.1, 0.2, 0.3, 0.4, 0.4]
+    
+    @IBOutlet weak var progressDataViewAll: ProgressDataView!
+    @IBOutlet weak var progressDataViewUc1: ProgressDataView!
+    @IBOutlet weak var progressDataViewUc2: ProgressDataView!
+    @IBOutlet weak var progressDataViewUc3: ProgressDataView!
+    @IBOutlet weak var progressDataViewUc4: ProgressDataView!
+    @IBOutlet weak var progressDataViewUc5: ProgressDataView!
+    @IBOutlet weak var averageTitleLabel: UILabel!
+    @IBOutlet weak var usecasesTitleLabel: UILabel!
+    @IBOutlet weak var explanationLabel: UILabel!
+    
     weak var coordinator: ComparisonTabCoordinator?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = NSLocalizedString("comparison_title", comment: "")
+        
+        explanationLabel.text = NSLocalizedString("compare_explanation", comment: "")
+        averageTitleLabel.text = NSLocalizedString("compare_title_average", comment: "")
+        usecasesTitleLabel.text = NSLocalizedString("compare_title_usecases", comment: "")
+        
+        progressDataViewAll.titleLabel.text = NSLocalizedString("compare_average", comment: "")
+        progressDataViewAll.progress = averageValue
+        
+        progressDataViewUc1.titleLabel.text = useCaseNames[0]
+        progressDataViewUc1.progress = useCaseValues[0]
+        
+        progressDataViewUc2.titleLabel.text = useCaseNames[1]
+        progressDataViewUc2.progress = useCaseValues[1]
+        
+        progressDataViewUc3.titleLabel.text = useCaseNames[2]
+        progressDataViewUc3.progress = useCaseValues[2]
+        
+        progressDataViewUc4.titleLabel.text = useCaseNames[3]
+        progressDataViewUc4.progress = useCaseValues[3]
+        
+        progressDataViewUc5.titleLabel.text = useCaseNames[4]
+        progressDataViewUc5.progress = useCaseValues[4]
     }
 }
---
+++ ios/Playground/Screens/Comparison/ProgressBar.swift
@@ -0,0 +1,70 @@
+//
+//  ProgressBar.swift
+//  Playground
+//
+//  Created by Fabian Heck on 16.12.19.
+//  Copyright © 2019 appcom interactive GmbH. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProgressBar : UIView {
+    
+    var progress: Float = 0 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+ 
+    
+    // initializer if UIView is created in the code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+    }
+    
+    // initializer if the UIView comes out of a storyboard
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpView()
+    }
+    
+    func setUpView() {
+         
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        drawBar(rect: rect, color: UIColor(#colorLiteral(red: 0.8980392157, green: 0.8980392157, blue: 0.8980392157, alpha: 1)))
+        drawBar(rect: rect, color: UIColor(#colorLiteral(red: 0.2470588235, green: 0.5803921569, blue: 0.9764705882, alpha: 1)), progress: progress)
+    }
+    
+    func drawBar(rect: CGRect, color: UIColor, progress: Float = 1) {
+        let height = min(bounds.size.height, 10)
+        let x = CGFloat(0)
+        let y = rect.height/2.0-height/2
+        var width = CGFloat(0)
+        if (0...1).contains(progress) {
+            width = bounds.size.width * CGFloat(progress)
+        }
+        
+        let radius = CGFloat(height/2)
+        let roundedRect = UIBezierPath(roundedRect: CGRect(x: x, y: y, width: width, height: height), cornerRadius: radius)
+        color.setFill()
+        roundedRect.fill()
+    }
+    
+    override func draw(_ layer: CALayer, in ctx: CGContext) {
+        super.draw(layer, in: ctx)
+    }
+    
+    /*
+    do sth with subviews when layout bounds change
+    called by the system after setNeedsLayout
+    */
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+}
